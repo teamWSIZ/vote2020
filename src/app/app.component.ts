372a4fb5aa95d28f37638ef9d79b51b816a7335c
asMEPH <pieRdole88>
2020-06-27 21:42:29 +0200

    wersja koncowa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,14 @@ export class AppComponent {
   constructor(private router: Router, private g: GService) {
   }
 
-  //podłączenie globalnej reakcji na klawisze
+  // podłączenie globalnej reakcji na klawisze
   @HostListener('window:keyup', ['$event'])
   keyEvent(event: KeyboardEvent) {
     console.log(JSON.stringify(event.key));
     if (event.key === '@') {
       // this.router.navigate(['adm']);
     } else if (event.shiftKey && event.key === 'Enter') {
-      //shift-enter włącza tryb admina
+      // shift-enter włącza tryb admina
     }
   }
 
